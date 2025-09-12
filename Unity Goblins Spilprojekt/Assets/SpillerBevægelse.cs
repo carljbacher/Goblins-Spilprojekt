6ad0311cdfad0ca101a4bfe7569de667c1efd15a
carljbacher <carljbacher@gmail.com>
2025-09-12 13:09:10 +0200

    Mere spil

--- Unity Goblins Spilprojekt/Assets/SpillerBevægelse.cs
+++ Unity Goblins Spilprojekt/Assets/SpillerBevægelse.cs
@@ -4,9 +4,11 @@ public class SpillerBevægelse : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float Speed;
+    [SerializeField] private float jumpPower;
     private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask groundLayer;
-
+    [SerializeField] private LayerMask wallLayer;
+    private float wallJumpCooldown;
 
     private void Awake()
     {
@@ -16,17 +18,52 @@ public class SpillerBevægelse : MonoBehaviour
 
     private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.linearVelocity.y);
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded())
+
+
+
+        // Wall jump logik
+        if (wallJumpCooldown > 0.2f)
         {
-            Jump();
+
+
+            body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.linearVelocity.y);
+
+            if (onWall() && !isGrounded())
+            {
+                body.gravityScale = 0;
+                body.linearVelocity = Vector2.zero;
+            }
+            else // Skal ændres hvis tyngdekraften bliver ændret
+            {
+                body.gravityScale = 3;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Jump();
+            }
+
+        }
+        else
+        {
+            wallJumpCooldown += Time.deltaTime;
         }
     }
 
     private void Jump()
     {
-        body.linearVelocity = new Vector2(body.linearVelocity.x, Speed);
+        if (isGrounded())
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, jumpPower);
+        }
+
+        else if (onWall() && !isGrounded())
+        {
+            wallJumpCooldown = 0;
+            body.linearVelocity = new Vector2(Mathf.Sign(transform.localScale.x) * 3, 10);
+            
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,6 +75,12 @@ public class SpillerBevægelse : MonoBehaviour
     private bool isGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+        private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
     }
 }
