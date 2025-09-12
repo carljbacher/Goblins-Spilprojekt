e28fcf042486ca74112b89482e51856c51bfe367
carljbacher <carljbacher@gmail.com>
2025-09-12 11:30:47 +0200

    Mere spil kode

--- Unity Goblins Spilprojekt/Assets/SpillerBevægelse.cs
+++ Unity Goblins Spilprojekt/Assets/SpillerBevægelse.cs
@@ -4,20 +4,40 @@ public class SpillerBevægelse : MonoBehaviour
 {
     private Rigidbody2D body;
     [SerializeField] private float Speed;
+    private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask groundLayer;
+
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * Speed, body.linearVelocity.y);
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
         {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, Speed);
+            Jump();
         }
     }
-    
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, Speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
 }
